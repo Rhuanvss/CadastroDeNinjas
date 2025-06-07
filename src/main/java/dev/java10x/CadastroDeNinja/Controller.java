d432faf6fca210b06d9eef5b95ac9d16c155118b
Rhuan Victor <rhuannvictor1323@gmail.com>
2025-06-07 00:01:32 -0300

    b

--- src/main/java/dev/java10x/CadastroDeNinja/Controller.java
+++ src/main/java/dev/java10x/CadastroDeNinja/Controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/boasvindas")
     public String boasVindas() {
-        return "Essa é minha primeira mensagem nessa rota, boas vindas";
+        return "Essa é minha primeira mensagem nessa rota, boas vindas 1";
     }
 }
