fff302ae11ccd7e6bb6e0fea15654d216ec94337
Rhuanvss <rhuanvictor1323@gmail.com>
2025-08-26 14:05:36 -0300

    alterar a rota do controlller

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,15 +1,50 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 
 public class NinjaController {
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
+
     @GetMapping("/boasvindas")
     public String boasvindas(){
         return "Essa é minha primeira mensagem ne   ssa porta";
+    }
+
+    //adicionar ninja
+    @PostMapping("/adicionar")
+    public String criarNinja(){
+        return "ninja criado";
+    }
+
+    //mostrar todos os ninja
+    @GetMapping("/todos")
+    public List<NinjaModel> listarNinjas(){
+        return ninjaService.ListarNinjas();
+    }
+
+    //mostrar ninja por id
+    @GetMapping("/todosID")
+    public List<NinjaModel> listarNinjasPorId(){
+        return ninjaService.listarNinjaPorId();
+    }
+    //alterar dados dos ninjas
+    @PutMapping("/alterarID")
+    public String alterarDadosDoNinja(){
+        return "alterar dados";
+    }
+
+    //deletar ninja
+    @DeleteMapping("/deletarID")
+    public String deletarNinja(){
+        return "deletar";
     }
 }
---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -0,0 +1,25 @@
+package dev.java10x.CadastroDeNinjas.Ninjas;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NinjaService {
+
+    private NinjaRepository ninjaRepository;
+
+    public NinjaService(NinjaRepository ninjaRepository) {
+        this.ninjaRepository = ninjaRepository;
+    }
+
+    // Listar Ninjas
+    public List<NinjaModel> ListarNinjas(){
+        return ninjaRepository.findAll();
+    }
+
+    //listar Ninja por ID
+    public List<NinjaModel> listarNinjaPorId(){
+        return ninjaRepository.findById();
+    }
+}
